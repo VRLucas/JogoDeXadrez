fabaccbe888e65869fcba61224563b59b099d80d
Lucas Souza <lukkita.lukkita@gmail.com>
2022-11-02 15:18:53 -0300

    Criacao da Classe BoardExpetion para tratamento de Expetion

---
+++ src/bordegame/BoardExption.java
@@ -0,0 +1,13 @@
+package bordegame;
+
+public class BoardExption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExption(String msg) {
+		super(msg);
+	}
+	
+	
+	
+
+}
