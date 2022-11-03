82dec75b6574e7a72ab49c803b0c0f581f0fccde
Lucas Souza <lukkita.lukkita@gmail.com>
2022-11-03 11:50:49 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,7 +56,21 @@ public class UI {
 			System.out.print((8 - i) + " ");
 
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+
+			}
+			System.out.println();
+
+		}
+		System.out.println("  a b c d e f g h");
+
+	}
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possivelMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j],possivelMoves[i][j]);
 
 			}
 			System.out.println();
@@ -66,9 +80,12 @@ public class UI {
 
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print( ANSI_RED_BACKGROUND);
+		}
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
